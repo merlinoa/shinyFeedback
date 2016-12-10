918a672d83c7d8b27168455b90647e5df134cbf6
merlinoa <merlinoa88@gmail.com>
2016-12-09 17:30:59 -0800

    added working warning danger and success wrappers for feedback()

--- R/feedback-wrappers.R
+++ R/feedback-wrappers.R
@@ -9,18 +9,82 @@
 #' @param color defailt \code{"#F89406"} the color of the feeback
 #' @param icon default \code{icon("warning-sign", lib="glyphicon"))} a \code{shiny::icon} object
 #' 
+#' @import shiny
+#' 
 #' @export
+#' 
 #' 
 feedbackWarning <- function(inputId, condition, 
                             text = "Ye be warned",
                             color = "#F89406", 
-                            icon = icon("warning-sign", lib="glyphicon")) {
+                            icon = shiny::icon("warning-sign", lib="glyphicon")) {
   
   feedback(
-    inputId = force(inputId),
-    condition = foce(condition),
-    text = force(text),
-    color = force(color),
-    icon = force(icon)
+    inputId = inputId,
+    condition = condition,
+    text = text,
+    color = color,
+    icon = icon
+  )
+}
+
+
+#' feedbackDanger
+#' 
+#' wrapper for \code{feedback()} function that displays a
+#' danger message by default
+#' 
+#' @param inputId the Shiny input's \code{inputId} argument
+#' @param condition condition under which feeback is displayed
+#' @param text default \code{"Ye be warned"} text string to display below input
+#' @param color defailt \code{"#F89406"} the color of the feeback
+#' @param icon default \code{icon("warning-sign", lib="glyphicon"))} a \code{shiny::icon} object
+#' 
+#' @import shiny
+#' 
+#' @export
+#' 
+#' 
+feedbackDanger <- function(inputId, condition, 
+                            text = "Danger, turn back!",
+                            color = "#d9534f", 
+                            icon = shiny::icon("exclamation-sign", lib="glyphicon")) {
+  
+  feedback(
+    inputId = inputId,
+    condition = condition,
+    text = text,
+    color = color,
+    icon = icon
+  )
+}
+
+#' feedbackSuccess
+#' 
+#' wrapper for \code{feedback()} function that displays a
+#' danger message by default
+#' 
+#' @param inputId the Shiny input's \code{inputId} argument
+#' @param condition condition under which feeback is displayed
+#' @param text default \code{"Ye be warned"} text string to display below input
+#' @param color defailt \code{"#F89406"} the color of the feeback
+#' @param icon default \code{icon("warning-sign", lib="glyphicon"))} a \code{shiny::icon} object
+#' 
+#' @import shiny
+#' 
+#' @export
+#' 
+#' 
+feedbackSuccess <- function(inputId, condition, 
+                            text = NULL,
+                            color = "#5cb85c", 
+                            icon = shiny::icon("ok", lib="glyphicon")) {
+  
+  feedback(
+    inputId = inputId,
+    condition = condition,
+    text = text,
+    color = color,
+    icon = icon
   )
 }
