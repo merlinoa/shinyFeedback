fb322fd7cf14cd80d3b59e5caf34d18b1422cf40
merlinoa <andy.merlino@tychobra.com>
2019-12-08 15:37:42 -0500

    `show/hideFeedback()` JS for `selectInput()`

--- inst/srcjs/showHideFeedback.js
+++ inst/srcjs/showHideFeedback.js
@@ -62,10 +62,80 @@
     
   }
   
+  var selectInputFeedback = {
+    "find": function(inputId) {
+      var input = findInput(inputId)
+      
+      var label = input.parent().siblings("label");
+      var formGroup = input.parent();
+      
+      // get the correct input element if selectize === true
+      if (input.hasClass("selectized") === true) {
+        
+        input = input.siblings(".selectize-control")
+          .find(".selectize-input");  
+        
+      }
+      
+    
+      return {
+        "input": input,
+        "label": label,
+        "formGroup": formGroup
+      }
+    },
+    
+    "hasFeedback": function(inputObject) {
+      
+      return inputObject.formGroup.hasClass("has-feedback")
+    },
+    
+    /* show the feedback along side the input
+    *
+    * @param message the `message` object sent from Shiny
+    * 
+    */
+    "show": function(inputObject, message) {
+      
+      var obj = inputObject 
+      
+      if (message.color) {
+        obj.label.css("color", message.color);
+        obj.input.css("border", "1px solid " + message.color);  
+      }
+      
+      if (message.text) {
+        $("<div id='" + message.inputId + "-text' class='col-xs-12'><p style='color: " + message.color +"; margin-top: 0px;'>"+ message.text +"</p>").insertAfter(obj.input);
+        obj.formGroup.append("</div><br id='" + message.inputId + "-spacing'/>");
+      }
+      
+      obj.formGroup.addClass("has-feedback");
+      if (message.icon) {
+        obj.input.parent().css("padding-right", 0);
+        $("<span id='" + message.inputId + "-icon' class='form-control-feedback' style='color: " + message.color + ";'>" + message.icon + "</span>").insertBefore(obj.input);
+      }
+    },
+    
+    "hide": function(inputObject, message) {
+      
+      var obj = inputObject
+      
+      obj.label.css("color", '');
+      obj.input.removeAttr("style");
+      
+      $("#" + message.inputId + "-icon").remove();
+      
+      inputObject.formGroup.removeClass("has-feedback");
+      
+      $("#" + message.inputId + "-text").remove();
+      $("#" + message.inputId + "-spacing").remove();
+    }
+    
+  }
   
   // all shiny input bindings that are supported by shinyFeedback
   var supportedInputs = [
-    //"shiny.selectInput",
+    {name: "shiny.selectInput", feedback: selectInputFeedback},
     //"shiny.dateInput",
     //"shiny.sliderInput",
     {name: "shiny.numberInput", feedback: textInputFeedback},
