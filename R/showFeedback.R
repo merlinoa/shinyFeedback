caa6d71a2344f0b1ec5c55cbd50261d265533296
merlinoa <andy.merlino@tychobra.com>
2019-12-09 08:53:30 -0500

    Document showFeedback variations in one file

--- R/showFeedback.R
+++ R/showFeedback.R
@@ -1,6 +1,6 @@
 #' showFeedback
 #' 
-#' show feedback next to Shiny input
+#' Show feedback next to Shiny inputs.
 #' 
 #' @param inputId the Shiny input's \code{inputId} argument
 #' @param text text string to display below input
@@ -9,8 +9,9 @@
 #' @param session the \code{shiny} session
 #' 
 #' @importFrom shiny getDefaultReactiveDomain
-#' 
-#' @seealso \code{\link{showFeedbackWarning}}, \code{\link{showFeedbackDanger}}, \code{\link{showFeedbackSuccess}}
+#'
+#' @rdname showFeedback
+#'   
 #' 
 #' @export
 #' 
@@ -39,6 +40,8 @@
 #'           color = "#d9534f",
 #'           icon = shiny::icon("exclamation-sign", lib="glyphicon")
 #'         )
+#'       } else {
+#'         hideFeedback("exampleInput")
 #'       }
 #'       
 #'     })
@@ -70,5 +73,112 @@ showFeedback <- function(
       color = color,
       icon = icon
     )
+  )
+}
+
+
+
+#' showFeedbackWarning
+#' 
+#' 
+#' @rdname showFeedback
+#' 
+#' @importFrom shiny icon
+#' 
+#' 
+#' @export
+#' @examples 
+#' 
+#' ## Only run examples in interacive R sessions
+#' if (interactive()) {
+#'   library(shiny)
+#'   
+#'   ui <- fluidPage(
+#'     useShinyFeedback(),
+#'     
+#'     numericInput(
+#'       "exampleInput",
+#'       "Show Feedback When < 0",
+#'       value = -5
+#'     )
+#'   )
+#'   
+#'   server <- function(input, output, session) {
+#'     observeEvent(input$exampleInput, {
+#'       
+#'       if (input$exampleInput < 0) {
+#'         showFeedbackWarning("exampleInput")
+#'       } else {
+#'         hideFeedback("exampleInput")
+#'       }
+#'       
+#'     })
+#'   }
+#'   
+#'   shinyApp(ui, server)
+#' }
+#' 
+showFeedbackWarning <- function(
+  inputId, 
+  text = "Ye be warned",
+  color = "#F89406", 
+  icon = shiny::icon("warning-sign", lib="glyphicon")
+) {
+  
+  showFeedback(
+    inputId = inputId,
+    text = text,
+    color = color,
+    icon = icon
+  )
+}
+
+
+#' showFeedbackDanger
+#' 
+#' 
+#' @importFrom shiny icon
+#' 
+#' @rdname showFeedback
+#' 
+#' @export
+#' 
+showFeedbackDanger <- function(
+  inputId,
+  text = "Danger, turn back!",
+  color = "#d9534f", 
+  icon = shiny::icon("exclamation-sign", lib="glyphicon")
+) {
+  
+  showFeedback(
+    inputId = inputId,
+    text = text,
+    color = color,
+    icon = icon
+  )
+}
+
+#' showFeedbackSuccess
+#' 
+#' 
+#' @importFrom shiny icon
+#'
+#' @rdname showFeedback
+#'
+#' @export
+#'
+#' 
+showFeedbackSuccess <- function(
+  inputId, 
+  text = NULL,
+  color = "#5cb85c", 
+  icon = shiny::icon("ok", lib="glyphicon")
+) {
+  
+  showFeedback(
+    inputId = inputId,
+    text = text,
+    color = color,
+    icon = icon
   )
 }
