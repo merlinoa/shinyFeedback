9335a6adae978d2edfd1f06398dc8a61848f0a4f
phoward38 <patrick.howard@tychobra.com>
2020-04-22 16:09:56 -0400

    Test app for loadingButton

---
+++ inst/examples/loadingButton_tests/ui.R
@@ -0,0 +1,33 @@
+ui <- fluidPage(
+  fluidRow(
+    column(
+      12,
+      tagList(
+        h1("Test App"),
+        loadingButton(
+          'btn',
+          'Loading Button'#,
+          # style = "color: black; background-color: green; border-color: green;",
+          # loadingStyle = "color: black; background-color: red; border-color: red;"
+        ),
+        actionButton(
+          'insert',
+          'Insert Loading Button'
+        )#,
+        # loadingButton(
+        #   'btn_2',
+        #   'Insert Button'
+        # ),
+        # actionButton(
+        #   'reset_2',
+        #   'Reset Insert'
+        # ),
+      )
+    )
+  )
+)
+
+# secure_ui(
+#   ui,
+#   firebase_config = config$firebase
+# )
