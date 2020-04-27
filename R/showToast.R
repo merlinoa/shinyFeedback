2caa1ddb27c62007e3923dc91e0f0ecc20295377
phoward38 <patrick.howard@tychobra.com>
2020-04-27 16:54:30 -0400

    showToast function (need to add R file w/ specific toast functions)

---
+++ R/showToast.R
@@ -0,0 +1,48 @@
+# custom configuration for the `shinytoastr::toastr_*()` functions
+toast_custom_defaults <- list(
+  position = "bottom-center",
+  progressBar = TRUE,
+  timeOut = 4000,
+  closeButton = TRUE,
+
+  # same as defaults
+  title = "",
+  newestOnTop = FALSE,
+  preventDuplicates = FALSE,
+  showDuration = 300,
+  hideDuration = 1000,
+  extendedTimeOut = 1000,
+  showEasing = c("swing", "linear"),
+  hideEasing = c("swing", "linear"),
+  showMethod = c("fadeIn", "slideDown", "show"),
+  hideMethod = c("fadeOut", "hide")
+)
+
+
+#' show toast message
+#'
+#' A wrapper around the `shinytoastr::toast_*()` functions that uses our preferred default argument
+#' values.
+#'
+#' @param type length 1 character vector.  Valid values are "success", "error", "warning", and "info"
+#' @param message the toast message
+#' @param .options other options to pass to the `shinytoastr::toast_*()` function
+#'
+#' @export
+#'
+#' @importFrom shinytoastr toastr_error toastr_success toastr_warning toastr_info
+#' @importFrom utils modifyList
+#'
+#' @return `invisible()`
+#'
+showToast <- function(type, message, .options = list()) {
+
+  args_list <- c(message, toast_custom_defaults)
+  args_out <- utils::modifyList(args_list, .options)
+
+  function_name <- paste0("shinytoastr::toastr_", type)
+
+  do.call(function_name, args_out)
+
+  invisible()
+}
