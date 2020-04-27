bf84452df161bbb6145d395255ae5378dd9893cf
phoward38 <patrick.howard@tychobra.com>
2020-04-27 16:53:26 -0400

    toastr dependency

--- R/dependencies.R
+++ R/dependencies.R
@@ -7,9 +7,6 @@ toastrDependency <- function() {
     src = "assets",
     package = "shinyFeedback",
     script = "js/toastr/toastr.min.js",
-    stylesheet = c("css/toastr/toastr.min.css")#, 
-    # "css/fontawesome/webfonts/fa-solid-900.ttf",
-    # "css/fontawesome/webfonts/fa-solid-900.woff",
-    # "css/fontawesome/webfonts/fa-solid-900.woff2")
+    stylesheet = "css/toastr/toastr.min.css"
   )
 }
